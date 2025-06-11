f6a39e2cb8a8414a3fdaecdf07416fa5b4610924
Michal Zielenkiewicz <michal@oasisprotocol.org>
2025-06-11 08:38:26 +0200

    Create app header

---
+++ src/components/Layout/Header.tsx
@@ -0,0 +1,20 @@
+import type { FC } from 'react';
+import { Link } from 'react-router';
+import { Button } from '@oasisprotocol/ui-library/src/components/ui/button';
+import { Wallet } from 'lucide-react';
+import Logotype from './logo.svg';
+
+export const Header: FC = () => {
+  return (
+    <header className="py-3 px-5 border-b w-full border-border/40">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link to="/">
+          <img src={Logotype} alt="Oasis ROFL" />
+        </Link>
+        <Button>
+          <Wallet /> Connect Wallet
+        </Button>
+      </div>
+    </header>
+  );
+};
