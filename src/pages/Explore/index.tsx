ba137bb8d81fa60dda4585285018709ce416a6fb
Michal Zielenkiewicz <michal@oasisprotocol.org>
2025-06-12 13:12:21 +0200

    Render content in Explore page

--- src/pages/Explore/index.tsx
+++ src/pages/Explore/index.tsx
@@ -1,11 +1,35 @@
-import type { FC } from 'react';
+import { type FC } from 'react';
 import { MainLayout } from '../../components/Layout/MainLayout';
 import { ExploreEmptyState } from './emptyState';
+import { useGetRuntimeRoflApps } from '../../nexus/api';
+import { useNetwork } from '../../hooks/useNetwork';
+import { Skeleton } from '@oasisprotocol/ui-library/src/components/ui/skeleton';
+import { ExploreAppCard } from './ExploreAppCard';
+
+const pageLimit = 18;
 
 export const Explore: FC = () => {
+  const network = useNetwork();
+  const roflAppsQuery = useGetRuntimeRoflApps(network, 'sapphire', {
+    limit: pageLimit,
+    offset: 0,
+  });
+  const { data, isLoading, isFetched } = roflAppsQuery;
+  const roflApps = data?.data.rofl_apps;
+
   return (
     <MainLayout>
-      <ExploreEmptyState />
+      {isFetched && !roflApps?.length && <ExploreEmptyState />}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {isLoading &&
+          Array.from({ length: pageLimit }).map((_, index) => (
+            <Skeleton key={index} className="w-full h-[200px]" />
+          ))}
+        {isFetched &&
+          roflApps?.map((app) => (
+            <ExploreAppCard key={app.id} app={app} network={network} />
+          ))}
+      </div>
     </MainLayout>
   );
 };
