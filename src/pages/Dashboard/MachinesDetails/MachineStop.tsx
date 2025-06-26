9328deb2e34e01b8350a6f066fdc0d865835aa16
Michal Zielenkiewicz <michal@oasisprotocol.org>
2025-06-26 12:51:18 +0200

    Enable machine stop action in view

--- src/pages/Dashboard/MachinesDetails/MachineStop.tsx
+++ src/pages/Dashboard/MachinesDetails/MachineStop.tsx
@@ -12,29 +12,36 @@ import {
 } from '@oasisprotocol/ui-library/src/components/ui/dialog'
 import { CircleStop } from 'lucide-react'
 
-export const MachineStop: FC = () => {
+type MachineStopProps = {
+  onConfirm?: () => void
+  disabled?: boolean
+}
+
+export const MachineStop: FC<MachineStopProps> = ({ onConfirm, disabled }) => {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button variant="outline" className="w-full md:w-auto md:mr-8">
+        <Button variant="outline" className="w-full md:w-auto md:mr-8" disabled={disabled}>
           <CircleStop />
           Stop
         </Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
-          <DialogTitle>Please confirm your action</DialogTitle>
+          <DialogTitle>Stop Machine</DialogTitle>
           <DialogDescription>
             This action will stop the machine and your app will not longer use this provider.
           </DialogDescription>
         </DialogHeader>
         <DialogFooter>
-          <DialogClose>
+          <DialogClose asChild>
             <Button variant="outline">Cancel</Button>
           </DialogClose>
-          <Button variant="destructive" onClick={() => console.log('trigger stop action')}>
-            Confirm
-          </Button>
+          <DialogClose asChild>
+            <Button variant="destructive" onClick={onConfirm}>
+              Confirm
+            </Button>
+          </DialogClose>
         </DialogFooter>
       </DialogContent>
     </Dialog>
