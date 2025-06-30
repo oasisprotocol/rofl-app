f446a1bd416eebfc427c5629c84e51d93c5ca62e
lukaw3d <luka@oasisprotocol.org>
2025-06-30 20:16:50 +0200

    Workaround to fix toast
    Error: Cannot read properties of undefined (reading 'ReactCurrentDispatcher')
    on ReactSharedInternals.ReactCurrentDispatcher.

--- src/components/RootLayout/index.tsx
+++ src/components/RootLayout/index.tsx
@@ -1,7 +1,25 @@
 import type { FC } from 'react'
 import { FathomAnalytics } from '../FathomAnalytics'
 import { Outlet } from 'react-router-dom'
-import { Toaster } from '@oasisprotocol/ui-library'
+import { Toaster as Sonner, ToasterProps } from 'sonner'
+
+// TODO: use import { Toaster } from '@oasisprotocol/ui-library' when it doesn't break
+const Toaster = ({ ...props }: ToasterProps) => {
+  return (
+    <Sonner
+      theme={'dark'}
+      className="toaster group"
+      style={
+        {
+          '--normal-bg': 'var(--popover)',
+          '--normal-text': 'var(--popover-foreground)',
+          '--normal-border': 'var(--border)',
+        } as React.CSSProperties
+      }
+      {...props}
+    />
+  )
+}
 
 export const RootLayout: FC = () => (
   <>
