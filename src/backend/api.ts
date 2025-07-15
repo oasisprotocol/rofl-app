4872d725689fa40e29703e11628c68a419c70703
lukaw3d <luka@oasisprotocol.org>
2025-07-15 02:30:42 +0200

    Artifact ids

--- src/backend/api.ts
+++ src/backend/api.ts
@@ -57,8 +57,14 @@ type RoflBuildResultsResponse = {
   enclaves: null | oasis.types.SGXEnclaveIdentity[] // Added in postprocessing
 }
 
+type ArtifactId =
+  | `${string}-rofl-template-yaml`
+  | `${string}-rofl-yaml`
+  | `${string}-compose-yaml`
+  | `${string}-readme-md`
+
 type ArtifactUploadRequest = {
-  id: string
+  id: ArtifactId
   file: File | Blob
 }
 
@@ -136,7 +142,7 @@ const uploadArtifact = async ({ id, file }: ArtifactUploadRequest, token: string
   })
 }
 
-const downloadArtifact = async (id: string, token: string): Promise<ArtifactDownloadResponse> => {
+const downloadArtifact = async (id: ArtifactId, token: string): Promise<ArtifactDownloadResponse> => {
   const response = await axios.get(`${BACKEND_URL}/artifacts/${id}`, {
     headers: {
       Authorization: `Bearer ${token}`,
@@ -183,7 +189,7 @@ export function useUploadArtifact(token: string | null) {
   })
 }
 
-export function useDownloadArtifact(id: string | null, token: string | null, enabled: boolean = true) {
+export function useDownloadArtifact(id: ArtifactId | null, token: string | null, enabled: boolean = true) {
   return useQuery<ArtifactDownloadResponse, AxiosError<unknown>>({
     queryKey: ['artifact-download', id, token],
     queryFn: () => downloadArtifact(id!, token!),
