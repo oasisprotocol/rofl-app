a49d42f6cbee2fd0811740db1eda3b4438871e27
Michal Zielenkiewicz <michal@oasisprotocol.org>
2025-09-01 10:47:51 +0200

    Add contact section to template selection

--- src/components/TemplatesList/index.tsx
+++ src/components/TemplatesList/index.tsx
@@ -19,55 +19,70 @@ export const TemplatesList: FC<TemplatesListProps> = ({ handleTemplateSelect }) 
   const filteredTemplates = templates.filter(template => template.id !== 'custom-build')
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6">
-      {customTemplate && (
-        <Card className="border-0 rounded-md">
-          <CardContent className="flex flex-col items-center justify-center h-full min-h-[300px] text-center space-y-2">
-            <CardTitle className="text-white text-lg">Custom Build</CardTitle>
-            <span className="flex flex-col gap-4 text-muted-foreground text-sm">
-              {customTemplate.description}
-              {handleTemplateSelect && (
-                <span>
-                  <Button onClick={() => handleTemplateSelect(customTemplate.id)}>
-                    <Plus className="h-4 w-4" />
-                  </Button>
-                </span>
-              )}
-            </span>
-          </CardContent>
-        </Card>
-      )}
+    <div className="flex flex-col gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6">
+        {customTemplate && (
+          <Card className="border-0 rounded-md">
+            <CardContent className="flex flex-col items-center justify-center h-full min-h-[300px] text-center space-y-2">
+              <CardTitle className="text-white text-lg">Custom Build</CardTitle>
+              <span className="flex flex-col gap-4 text-muted-foreground text-sm">
+                {customTemplate.description}
+                {handleTemplateSelect && (
+                  <span>
+                    <Button onClick={() => handleTemplateSelect(customTemplate.id)}>
+                      <Plus className="h-4 w-4" />
+                    </Button>
+                  </span>
+                )}
+              </span>
+            </CardContent>
+          </Card>
+        )}
 
-      {filteredTemplates.map(template => (
-        <Card key={template.id} className="rounded-md pt-6 flex flex-col">
-          <div className="rounded-t-md h-[160px] -mt-6">
-            <img
-              src={template.image}
-              alt={template.name}
-              className="w-full h-full object-cover rounded-t-md"
-            />
-          </div>
-          <CardHeader className="gap-0">
-            <CardTitle className="text-white text-lg">{template.name}</CardTitle>
-          </CardHeader>
-          <CardContent className="space-y-4 flex-grow">
-            <span className="text-muted-foreground text-sm">
-              {template.description || 'No description available.'}
-            </span>
-          </CardContent>
-          {handleTemplateSelect && (
-            <CardFooter>
-              <Button
-                variant="secondary"
-                className="w-full"
-                onClick={() => handleTemplateSelect(template.id)}
-              >
-                Select
-              </Button>
-            </CardFooter>
-          )}
-        </Card>
-      ))}
+        {filteredTemplates.map(template => (
+          <Card key={template.id} className="rounded-md pt-6 flex flex-col">
+            <div className="rounded-t-md h-[160px] -mt-6">
+              <img
+                src={template.image}
+                alt={template.name}
+                className="w-full h-full object-cover rounded-t-md"
+              />
+            </div>
+            <CardHeader className="gap-0">
+              <CardTitle className="text-white text-lg">{template.name}</CardTitle>
+            </CardHeader>
+            <CardContent className="space-y-4 flex-grow">
+              <span className="text-muted-foreground text-sm">
+                {template.description || 'No description available.'}
+              </span>
+            </CardContent>
+            {handleTemplateSelect && (
+              <CardFooter>
+                <Button
+                  variant="secondary"
+                  className="w-full"
+                  onClick={() => handleTemplateSelect(template.id)}
+                >
+                  Select
+                </Button>
+              </CardFooter>
+            )}
+          </Card>
+        ))}
+      </div>
+
+      <div className="flex flex-col items-center justify-center p-6 border rounded-lg gap-4">
+        <div className="flex flex-col items-center gap-2 w-full">
+          <h3 className="text-card-foreground text-base font-semibold">Got your own ideas?</h3>
+          <p className="text-muted-foreground text-sm">
+            Contact us and suggest it so we can work with you implementing it.
+          </p>
+        </div>
+
+        <Button variant="outline" className="!bg-background" asChild>
+          <a href="mailto:#">Send E-mail</a>
+        </Button>
+      </div>
     </div>
   )
 }
