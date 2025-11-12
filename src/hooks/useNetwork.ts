f597ce779e4151b623b2043d9a4e324a933f033f
lubej <matej.lubej@oasisprotocol.org>
2025-11-12 08:53:39 +0100

    Make sure ROFL Paymaster chain do not throw
    - update hidden chains in chain dialog

--- src/hooks/useNetwork.ts
+++ src/hooks/useNetwork.ts
@@ -2,6 +2,7 @@ import { useAccount } from 'wagmi'
 import { AppError, AppErrors } from '../components/ErrorBoundary/errors'
 import { sapphire, sapphireTestnet } from 'viem/chains'
 import { useRef } from 'react'
+import { ROFL_PAYMASTER_ENABLED_CHAINS_IDS } from '../constants/rofl-paymaster-config.ts'
 
 export function useNetwork(fallback?: 'mainnet' | 'testnet') {
   const { isConnected, chainId } = useAccount()
@@ -23,6 +24,11 @@ export function useNetwork(fallback?: 'mainnet' | 'testnet') {
   if (chainId === sapphireTestnet.id) {
     previousValueRef.current = 'testnet'
     return 'testnet'
+  }
+
+  // Allow payment-required chains without throwing errors
+  if (chainId && ROFL_PAYMASTER_ENABLED_CHAINS_IDS.includes(chainId.toString())) {
+    if (previousValueRef.current) return previousValueRef.current
   }
 
   if (fallback) {
