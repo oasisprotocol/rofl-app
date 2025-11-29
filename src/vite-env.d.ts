5e94643e6f8b75b3578a0c8343292515cc44288a
lubej <matej.lubej@oasisprotocol.org>
2025-11-29 13:01:51 +0100

    Disable paymaster testnet integration in prod

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -9,6 +9,7 @@ interface ImportMetaEnv {
   VITE_WALLET_CONNECT_PROJECT_ID: string
   VITE_ROFL_APP_BACKEND: string
   VITE_FATHOM_SIDE_ID: string
+  VITE_FEATURE_FLAG_PAYMASTER: string
 }
 
 interface ImportMeta {
