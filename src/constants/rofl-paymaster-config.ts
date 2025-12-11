9c1b66000b5087803dcf4439091f56e9d3200c1d
lubej <matej.lubej@oasisprotocol.org>
2025-12-11 09:53:13 +0100

    Update ROFL Paymaster config for mainnet

--- src/constants/rofl-paymaster-config.ts
+++ src/constants/rofl-paymaster-config.ts
@@ -1,15 +1,15 @@
-import { sapphire, sapphireTestnet, sepolia } from 'viem/chains'
+import { sapphire, sapphireTestnet, base } from 'viem/chains'
 import { Address } from 'viem'
 
-export const ROFL_PAYMASTER_ENABLED_CHAINS = [sepolia]
+export const ROFL_PAYMASTER_ENABLED_CHAINS = [base]
 export const ROFL_PAYMASTER_ENABLED_CHAINS_IDS = ROFL_PAYMASTER_ENABLED_CHAINS.map(chain =>
   chain.id.toString(),
 )
 export const ROFL_PAYMASTER_NATIVE_TOKEN_ADDRESS = '0xNATIVE'
 export const ROFL_PAYMASTER_EXPECTED_TIME = 30 // 30 seconds
 export const ROFL_PAYMASTER_DEPOSIT_GAS_LIMIT = 500_000n
-export const ROFL_PAYMASTER_DESTINATION_CHAIN = sapphireTestnet
-export const ROFL_PAYMASTER_DESTINATION_CHAIN_TOKEN = sapphireTestnet.nativeCurrency
+export const ROFL_PAYMASTER_DESTINATION_CHAIN = sapphire
+export const ROFL_PAYMASTER_DESTINATION_CHAIN_TOKEN = sapphire.nativeCurrency
 
 type RoflPaymasterTokenConfig = {
   [chainId: string]: {
@@ -24,21 +24,20 @@ type RoflPaymasterTokenConfig = {
 }
 
 export const ROFL_PAYMASTER_TOKEN_CONFIG: RoflPaymasterTokenConfig = {
-  [sepolia.id]: {
-    paymasterContractAddress: '0x7304cAc43536CAE229c0D55129B867CCF43F83F4',
+  [base.id]: {
+    paymasterContractAddress: '0x7D3B4dd07bd523E519e0A91afD8e3B325586fb5b',
     TOKENS: [
       {
-        contractAddress: '0xaA8E23Fb1079EA71e0a56F48a2aA51851D8433D0',
-        symbol: 'USDT',
+        contractAddress: '0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913',
+        symbol: 'USDC',
         decimals: 6,
-        name: 'Tether USD',
+        name: 'USDC',
       },
     ],
   },
 }
 
 export const ROFL_PAYMASTER_SAPPHIRE_CONTRACT_CONFIG = {
-  // TODO: replace with actual address once deployed on mainnet
-  [sapphire.id]: '0x0000000000000000000000000000000000000000' as Address,
+  [sapphire.id]: '0x6997953a4458F019506370110e84eefF52d375ad' as Address,
   [sapphireTestnet.id]: '0xa26733606bf8e0bD8d77Bddb707F05d7708EfBf7' as Address,
 }
