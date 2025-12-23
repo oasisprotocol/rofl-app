4882f3ce438db1ff7b313ef2925266fb00bdb13d
lukaw3d <luka@oasisprotocol.org>
2025-12-23 02:24:33 +0100

    Display who can view logs (EVM addresses)

--- src/pages/Dashboard/MachinesDetails/WhoCanViewLogs.tsx
+++ src/pages/Dashboard/MachinesDetails/WhoCanViewLogs.tsx
@@ -1,6 +1,30 @@
 import { type FC } from 'react'
 import * as oasis from '@oasisprotocol/client'
 import type { RoflMarketInstance } from '../../../nexus/api'
+import { useGetRuntimeAccountsAddress } from '../../../nexus/api'
+import { useNetwork } from '../../../hooks/useNetwork'
+import { getEthAccountAddressFromPreimage } from '../../../utils/helpers'
+
+const DisplayEvmAddressFromOasis1: FC<{
+  oasisAddress: string
+}> = ({ oasisAddress }) => {
+  const network = useNetwork()
+  const accountQuery = useGetRuntimeAccountsAddress(network, 'sapphire', oasisAddress, {
+    // @ts-expect-error Incorrect type demands queryKey
+    query: {
+      enabled: !!oasisAddress,
+    },
+  })
+
+  const evmAddress = accountQuery.data?.data?.address_preimage
+    ? getEthAccountAddressFromPreimage(accountQuery.data.data.address_preimage)
+    : undefined
+
+  if (!evmAddress) {
+    return <div className="text-sm">{oasisAddress} - couldn't find corresponding 0x... EVM address</div>
+  }
+  return <div className="text-sm">{evmAddress}</div>
+}
 
 export const WhoCanViewLogs: FC<{
   machine: RoflMarketInstance
@@ -8,9 +32,11 @@ export const WhoCanViewLogs: FC<{
   const metadata = machine.deployment?.metadata as undefined | { 'net.oasis.scheduler.permissions'?: string }
 
   try {
-    const permissions = metadata?.['net.oasis.scheduler.permissions'] ? oasis.misc.fromCBOR(
-      oasis.misc.fromBase64(metadata['net.oasis.scheduler.permissions']),
-    ) as { 'log.view'?: Uint8Array[] } : {}
+    const permissions = metadata?.['net.oasis.scheduler.permissions']
+      ? (oasis.misc.fromCBOR(oasis.misc.fromBase64(metadata['net.oasis.scheduler.permissions'])) as {
+          'log.view'?: Uint8Array[]
+        })
+      : {}
 
     const viewAccounts = permissions['log.view'] || []
     return (
@@ -19,9 +45,10 @@ export const WhoCanViewLogs: FC<{
         <div className="text-sm">Admin</div>
         <div className="text-sm">Node provider</div>
         {viewAccounts.map((addressBytes, index) => (
-          <div key={index} className="text-sm">
-            {oasis.staking.addressToBech32(addressBytes)}
-          </div>
+          <DisplayEvmAddressFromOasis1
+            key={index}
+            oasisAddress={oasis.staking.addressToBech32(addressBytes)}
+          />
         ))}
       </div>
     )
