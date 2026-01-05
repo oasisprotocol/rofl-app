cd5e86f052300e4d9d05f87a78c9c5cdef1994d9
lubej <matej.lubej@oasisprotocol.org>
2026-01-05 16:44:45 +0100

    Update paymaster config

--- src/constants/rofl-paymaster-config.ts
+++ src/constants/rofl-paymaster-config.ts
@@ -1,4 +1,4 @@
-import { sapphire, sapphireTestnet, base } from 'viem/chains'
+import { sapphire, base, sapphireTestnet } from 'wagmi/chains'
 import { Address } from 'viem'
 
 export const ROFL_PAYMASTER_ENABLED_CHAINS = [base]
@@ -6,24 +6,23 @@ export const ROFL_PAYMASTER_ENABLED_CHAINS_IDS = ROFL_PAYMASTER_ENABLED_CHAINS.m
   chain.id.toString(),
 )
 export const ROFL_PAYMASTER_NATIVE_TOKEN_ADDRESS = '0xNATIVE'
-export const ROFL_PAYMASTER_EXPECTED_TIME = 30 // 30 seconds
+export const ROFL_PAYMASTER_EXPECTED_TIME = 60 // 60 seconds
 export const ROFL_PAYMASTER_DEPOSIT_GAS_LIMIT = 500_000n
 export const ROFL_PAYMASTER_DESTINATION_CHAIN = sapphire
 export const ROFL_PAYMASTER_DESTINATION_CHAIN_TOKEN = sapphire.nativeCurrency
 
-type RoflPaymasterTokenConfig = {
-  [chainId: string]: {
-    paymasterContractAddress: Address
-    TOKENS: {
-      contractAddress: Address
-      symbol: string
-      decimals: number
-      name: string
-    }[]
-  }
+export type RoflPaymasterTokenConfig = {
+  contractAddress: Address
+  symbol: string
+  decimals: number
+  name: string
+}
+export type RoflPaymasterChainConfig = {
+  paymasterContractAddress: Address
+  TOKENS: RoflPaymasterTokenConfig[]
 }
 
-export const ROFL_PAYMASTER_TOKEN_CONFIG: RoflPaymasterTokenConfig = {
+export const ROFL_PAYMASTER_TOKEN_CONFIG: Record<string, RoflPaymasterChainConfig> = {
   [base.id]: {
     paymasterContractAddress: '0x7D3B4dd07bd523E519e0A91afD8e3B325586fb5b',
     TOKENS: [
@@ -33,6 +32,12 @@ export const ROFL_PAYMASTER_TOKEN_CONFIG: RoflPaymasterTokenConfig = {
         decimals: 6,
         name: 'USDC',
       },
+      {
+        contractAddress: '0xfde4C96c8593536E31F229EA8f37b2ADa2699bb2',
+        symbol: 'USDT',
+        decimals: 6,
+        name: 'Tether USD',
+      },
     ],
   },
 }
@@ -41,3 +46,5 @@ export const ROFL_PAYMASTER_SAPPHIRE_CONTRACT_CONFIG = {
   [sapphire.id]: '0x6997953a4458F019506370110e84eefF52d375ad' as Address,
   [sapphireTestnet.id]: '0xa26733606bf8e0bD8d77Bddb707F05d7708EfBf7' as Address,
 }
+
+export const ROFL_PAYMASTER_MIN_USD_VALUE = 1
