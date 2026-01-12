6a33ae12248528f564dfec8d809a2605fb5f41f4
lubej <matej.lubej@oasisprotocol.org>
2026-01-12 14:32:31 +0100

    Update RPC url

--- src/constants/rofl-8004.ts
+++ src/constants/rofl-8004.ts
@@ -11,8 +11,8 @@ export const ROFL_8004_METADATA_KEY = 'net.oasis.app.erc8004_agent_id' // "{chai
 export const ROFL_8004_SUPPORTED_CHAINS = {
   [sepolia.id.toString()]: {
     chain: sepolia,
-    rpcUrl: sepolia.rpcUrls.default.http,
     identityContract: '0x8004a6090cd10a7288092483047b097295fb8847',
+    rpcUrl: ['https://ethereum-sepolia-rpc.publicnode.com'],
     validatorAddress: '0x7cd2eccd146b6626c8a500ab5644b1a506115e6f',
   },
 }
